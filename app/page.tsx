c9b2e89788ac7e388c3669378c5832ff44d9c959
shawn-dsz <shawndsouza@outlook.com>
2023-08-03 13:09:42 +1000

    handling errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
+import { ErrorBoundary } from "react-error-boundary";
 import FoodList from "./_components/FoodList";
 import FoodProvider from "./_components/FoodProvider";
 
 export default function Home() {
   const foodRequest = fetch("http:localhost:3000/api/foods", {
     cache: "no-cache",
-  }).then((res) => res.json());
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      throw new Error("oops");
+    });
 
   return (
-    <FoodProvider foodPromise={foodRequest}>
-      <main className="max-w-6xl mx-auto mt-5 text-3xl">
-        <FoodList />
-        <FoodList />
-        <FoodList />
-        <FoodList />
-      </main>
-    </FoodProvider>
+    <ErrorBoundary fallback={<div>something went wrong</div>}>
+      <FoodProvider foodPromise={foodRequest}>
+        <main className="max-w-6xl mx-auto mt-5 text-3xl">
+          <FoodList />
+          <FoodList />
+          <FoodList />
+          <FoodList />
+        </main>
+      </FoodProvider>
+    </ErrorBoundary>
   );
 }
